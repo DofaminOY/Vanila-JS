2cf8f6de708cd91751ec09e1dfd13db9be8ebbb2
Yura Ostrovsky <yurkasoft@gmail.com>
2021-04-10 13:04:49 +0300

    start

--- js/scrip.js
+++ js/scrip.js
@@ -1,343 +1,77 @@
 window.addEventListener('DOMContentLoaded', () => {
-    // Tabs
+       // Calculator
 
-    let tabs = document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items');
+       const result = document.querySelector('.calculating__result span');
+       let calc = quantity, commission, costWeek, quantitySubscriptions, middleWeek, noPay, cpi, returnPay;
+       
+       
+       function calcTotal() {
+        
+           if (!quantity || !commission || !costWeek || !quantitySubscriptions || !middleWeek || !noPay || !cpi || !returnPay) {
+               result.textContent = '____'; // Можете придумать что угодно
+               return;
+           } else {
+               
+               result.textContent = quantity / ((((costWeek * middleWeek) * quantitySubscriptions) / 100) - (((100 - noPay) * cpi) / 100) - ((((costWeek * middleWeek) * quantitySubscriptions) * (returnPay / 100)) / 100) - ((((costWeek * middleWeek) * quantitySubscriptions) * (commission / 100)) / 100)) 
+              
+            //   (((costWeek * middleWeek) * quantitySubscriptions) / 100)
+            //   (((100 - noPay) * cpi) / 100)
+            //   ((((costWeek * middleWeek) * quantitySubscriptions) * (returnPay / 100)) / 100)
+            //   ((((costWeek * middleWeek) * quantitySubscriptions) * (commission / 100)) / 100)
+            //   quantity / ()  
 
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
-
-        /* tabsContent.forEach(item => {
-             item.style.display = 'none';
-         });
-         */
-
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        });
-    }
-
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-    }
-
-    // tabsContent[i].style.display = "block";
-    // tabs[i].classList.add('tabheader__item_active');
-    hideTabContent();
-    showTabContent();
-    tabsParent.addEventListener('click', (event) => {
-        const target = event.target;
-
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
-        }
-    });
-    //Timer
-
-    const deadline = '2021-03-20';
-
-    function getTimeRemaining(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
-            days = Math.floor((t / (1000 * 60 * 60 * 24))),
-            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            seconds = Math.floor((t / 1000) % 60);
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-
-        };
-    }
-
-    function getZero(num) {
-        if (num >= 0 && num < 10) {
-            return '0' + num;
-        } else {
-            return num;
-        }
-    }
-
-    function solution(number) {
-        let result = "";
-        let transform = function (value, roman) {
-            while (number >= value) {
-                result += roman;
-                number -= value;
-
-            }
-
-        };
-
-        transform(1000, "M");
-        transform(900, "CM");
-        transform(500, "D");
-        transform(400, "CD");
-        transform(100, "C");
-        transform(90, "XC");
-        transform(50, "L");
-        transform(40, "XL");
-        transform(10, "X");
-        transform(9, "IX");
-        transform(5, "V");
-        transform(4, "IV");
-        transform(1, "I");
-
-
-        return result;
-    }
-
-    function setClock(selector, endtime) {
-        const timer = document.querySelector(selector),
-            days = timer.querySelector('#days'),
-            hours = timer.querySelector('#hours'),
-            minutes = timer.querySelector('#minutes'),
-            seconds = timer.querySelector('#seconds'),
-            timeInterval = setInterval(updateClock, 1000);
-
-        updateClock();
-
-        function updateClock() {
-
-            const t = getTimeRemaining(endtime);
-
-            days.innerHTML = solution(t.days);
-            hours.innerHTML = getZero(t.hours);
-            minutes.innerHTML = getZero(t.minutes);
-            seconds.innerHTML = getZero(t.seconds);
-
-            if (t.total <= 0) {
-                clearInterval(timeInterval);
-            }
-
-        }
-    }
-    setClock('.timer', deadline);
-
-    // Modal
-
-    const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal');
-
-    function openModal() {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        //modal.classList.toggle('show');
-        document.body.style.overflow = 'hidden';
-        clearInterval(modalTimerId);
-    }
-
-    modalTrigger.forEach(btn => {
-        btn.addEventListener('click', openModal);
-    });
-
-    function closeModal() {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
-        //modal.classList.toggle('show');
-        document.body.style.overflow = '';
-    }
-
-
-    modal.addEventListener('click', (e) => {
-        if (e.target === modal || e.target.getAttribute('data-close') == '') {
-            closeModal();
-        }
-    });
-
-    document.addEventListener('keydown', (e) => {
-        if (e.code === "Escape" && modal.classList.remove('show')) {
-            closeModal();
-        }
-    });
-
-    const modalTimerId = setTimeout(openModal, 50000);
-
-    function showModalByScroll() {
-        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-            openModal();
-            window.removeEventListener('scroll', showModalByScroll);
-        }
-    }
-
-    window.addEventListener('scroll', showModalByScroll);
+           }
+       }
+   
+       calcTotal();
+   
     
-
-    // классы для карточок.
-
-
-
-    class MenuCard {
-        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-            this.src = src;
-            this.alt = alt;
-            this.title = title;
-            this.descr = descr;
-            this.price = price;
-            this.classes = classes;
-            this.parent = document.querySelector(parentSelector);
-            this.transfer = 27;
-            this.changeToUAH();
-        }
-        changeToUAH() {
-            this.price = +this.price * this.transfer;
-        }
-        render() {
-            const element = document.createElement('div');
-            if (this.classes.length === 0) {
-                this.element = 'menu__item';
-                element.classList.add(this.element);
-
-            } else {
-                this.classes.forEach(className => element.classList.add(className));
-            }
-
-            element.innerHTML = `
-           
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.title}</h3>
-                    <div class="menu__item-descr">${this.descr}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
-            `;
-            this.parent.append(element);
-        }
-    }
-    // const div = new MenuCard();
-    // div.render();
-    new MenuCard(
-        'img/tabs/vegy.jpg',
-        'vegy',
-        'Меню "Фитнес"',
-        `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих
-        овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной
-        ценой и высоким качеством!`,
-        9,
-        '.menu .container',
-
-
-
-    ).render();
-
-    new MenuCard(
-        'img/tabs/elite.jpg',
-        'elite',
-        'Меню "Премиум"',
-        `В меню “Премиум” мы используем не только красивый дизайн упаковки, но
-        и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода
-        в ресторан!`,
-        13,
-        '.menu .container',
-        'menu__item',
-        'big'
-
-
-    ).render();
-    new MenuCard(
-        'img/tabs/post.jpg',
-        'post',
-        'Меню "Постное"',
-        `Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие
-        продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное
-        количество белков за счет тофу и импортных вегетарианских стейков.`,
-        26,
-        '.menu .container',
-        'menu__item',
-        'big'
-
-
-    ).render();
-
-
-    // Forms
-
-   const forms = document.querySelectorAll('form');
-
-    const message = {
-        loading: 'img/form/spinner.svg',
-        success: 'Спасибо! Скоро мы с вами свяжемся',
-        failure: 'Что-то пошло не так...'
-    };
-    forms.forEach(item => {
-        postData(item);
-    });
-
-    function postData(form) {
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-
-            let statusMessage = document.createElement('img');
-            statusMessage.src = message.loading;
-            statusMessage.style.cssText = `
-                display: block;
-                margin: 0 auto;
-            `;
-            form.insertAdjacentElement('afterend', statusMessage);
-
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-            const formData = new FormData(form);
-
-            const object = {};
-            formData.forEach(function (value, key) {
-                object[key] = value;
-            });
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
-
-        });
-    }
-
-    function showThanksModal(message) {
-        const prevModalDialog = document.querySelector('.modal__dialog');
-        prevModalDialog.classList.add('hide');
-        openModal();
-
-        const thanksModal = document.createElement('div');
-        thanksModal.classList.add('modal__dialog');
-        thanksModal.innerHTML = `
-        <div class="modal__content">
-        <div class="modal__close" data-close>×</div>
-        <div class="modal__title">${message}</div>
-        </div>
-        `;
-        document.querySelector('.modal').append(thanksModal);
-        setTimeout(() => {
-            thanksModal.remove();
-            prevModalDialog.classList.add('show');
-            prevModalDialog.classList.remove('hide');
-            closeModal();
-        }, 400000);
-    }
-
+   
+       
+       
+   
+       function getDynamicInformation(selector) {
+           const input = document.querySelector(selector);
+   
+           input.addEventListener('input', () => {
+               switch(input.getAttribute('id')) {
+                   case "quantity":
+                    quantity = +input.value;
+                       break;
+                   case "commission":
+                    commission = +input.value;
+                       break;
+                   case "costWeek":
+                    costWeek = +input.value;
+                       break;
+                   case "quantitySubscriptions":
+                    quantitySubscriptions = +input.value;
+                        break;
+                   case "middleWeek":
+                    middleWeek = +input.value;
+                        break;
+                   case "noPay":
+                    noPay = +input.value;
+                        break;
+                   case "cpi":
+                    cpi = +input.value;
+                        break;
+                   case "returnPay":
+                    returnPay = +input.value;
+                        break;
+               }
+   
+               calcTotal();
+           });
+       }
+   
+       getDynamicInformation('#quantity');
+       getDynamicInformation('#commission');
+       getDynamicInformation('#costWeek');
+       getDynamicInformation('#quantitySubscriptions');
+       getDynamicInformation('#middleWeek');
+       getDynamicInformation('#noPay');
+       getDynamicInformation('#cpi');
+       getDynamicInformation('#returnPay');
+   
 });
